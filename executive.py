54c153ccbad64277f590df2531bb87aed16a22a8
Andysword <andrewhuang2019@gmail.com>
2023-11-04 16:40:24 -0500

    Bugged Python Files
    These files are bugged for some reason...

--- executive.py
+++ executive.py
@@ -2,18 +2,12 @@
 
 from model import Model
 
+
 class Executive:
     def __init__(self):
-        self.model = None
+        self.model = Model()
     
     def run(self):
-        self.read_file()
+        self.model.read_file()
+        self.model.create_model_outcomes()
         
-    
-    def read_file(self):
-        file_name = input("Please enter a data file: ")
-        data_file = open(file_name, "r")
-    
-    def run_model(self, data_set):
-        self.model = Model(data_set)
-        return self.model.create_model_outcomes()
--- model.py
+++ model.py
@@ -1,10 +1,22 @@
 # model.py
 
+import pandas as pd
+
+
 class Model:
-    def __init__(self, data_set):
-        self.data_set = data_set
+    
+    def __init__(self):
+        self.file_name = None
+        self.file_name2 = None
+        self.file_name3 = None
+        
+    def read_file(self):
+        self.file_name = input("Please enter leak locations file: ")
+        self.file_name2 = input("Please enter sensor readings file: ")
+        self.file_name3 = input("Please enter weather data file: ")
     
     def create_model_outcomes(self):
-        for line in self.data_set:
-            file = self.data_set.readline()
+        leak_locations_data = pd.read_csv(self.file_name)
+        leak_locations_data.describe()
+        
         
