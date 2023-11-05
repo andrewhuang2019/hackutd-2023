d33116532a23a5158acc59af95974b6a8be3b3da
Andysword <andrewhuang2019@gmail.com>
2023-11-04 20:01:30 -0500

    General changes in model
    Commented out two of the data files in Model. Began the development of the model

--- model.py
+++ model.py
@@ -12,11 +12,12 @@ class Model:
         
     def read_file(self):
         self.file_name = input("Please enter leak locations file: ")
-        self.file_name2 = input("Please enter sensor readings file: ")
-        self.file_name3 = input("Please enter weather data file: ")
+        #self.file_name2 = input("Please enter sensor readings file: ")
+        #self.file_name3 = input("Please enter weather data file: ")
     
     def create_model_outcomes(self):
         leak_locations_data = pd.read_csv(self.file_name)
-        leak_locations_data.describe()
-        
-        
+        print(leak_locations_data.columns)
+        y = leak_locations_data.LeakRate
+        features = ['NumberSourcesLeaking, Latitude, Longitude, EmissionCategory, Duration']
+        X = leak_locations_data[features]
