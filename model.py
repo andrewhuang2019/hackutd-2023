c073ee027f27055fc5cba1aa6d7d85b3e11d2063
Andysword <andrewhuang2019@gmail.com>
2023-11-05 06:49:47 -0600

    Cleaning
    Got rid of dependencies that were not used

--- model.py
+++ model.py
@@ -2,8 +2,6 @@
 
 import pandas as pd
 import statistics
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import HistGradientBoostingRegressor
