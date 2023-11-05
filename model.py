80e592647ccedc0faf48f8566a9b27f58733c441
Andysword <andrewhuang2019@gmail.com>
2023-11-04 23:08:13 -0500

    Model Adding files
    Working with the other 3 files and trying to combine them for the same model

--- model.py
+++ model.py
@@ -2,28 +2,40 @@
 
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import HistGradientBoostingRegressor
 
 
 class Model:
     
     def __init__(self):
-        self.file_name = None
+        self.file_name1 = None
         self.file_name2 = None
         self.file_name3 = None
         
     # reads the input files and stores them in global variables
     def read_file(self):
-        self.file_name = input("Please enter leak locations file: ")
+        #self.file_name = input("Please enter leak locations file: ")
         #self.file_name2 = input("Please enter sensor readings file: ")
         #self.file_name3 = input("Please enter weather data file: ")
+        
+        self.file_name1 = "leak_locations_and_rate.csv"
+        self.file_name2 = "sensor_readings.csv"
+        self.file_name3 = "weather_data.csv"
     
     # creates the model and returns the outcomes of the model with the input files
     def create_model_outcomes(self):
         
         # saves the data from the files into a table
-        leak_locations_data = pd.read_csv(self.file_name)
+        leak_locations_data = pd.read_csv(self.file_name1)
+        sensor_readings_data = pd.read_csv(self.file_name2)
+        weather_data = pd.read_csv(self.file_name3)
+        
+        combined_data = pd.concat([leak_locations_data, sensor_readings_data, weather_data])
+        
+        print(combined_data.head().describe())
 
         # shuffle entire dataset
         # leak_locations_data = leak_locations_data.sample(frac = 1)
@@ -32,40 +44,30 @@ class Model:
         leak_rate_column = leak_locations_data.LeakRate
 
         # chooses the specific features to compare data with 
-        features = ['NumberSourcesLeaking', 'Latitude', 'Longitude', 'Duration']
+        features = ['NumberSourcesLeaking', 'Latitude', 'Longitude', 'Duration', 'Barometric_Pressure', 'Humidity', 'Temperature', 'Wind_Direction', 'Wind_Speed']
         
         # creates a table with only the chosen data
-        feature_data = leak_locations_data[features]
+        feature_data = combined_data[features]
         
         # split 20% of both rows into training data
-        train_X, test_X, train_y, test_y = train_test_split(feature_data, leak_rate_column, test_size = 0.2, random_state = 0)
+        train_X, test_X, train_y, test_y = train_test_split(feature_data, leak_rate_column, test_size = 0.2, random_state = 1)
         
         # creates a decision tree regressor and creates a prediction
         
         
-        leak_locations_model = DecisionTreeRegressor(random_state = 0)
+        #leak_locations_model = DecisionTreeRegressor(random_state = 0)
+        #leak_locations_model.fit(train_X, train_y)
+        
+        #predicted_leak_speed = leak_locations_model.predict(test_X)
+        
+        #print("MAE:", mean_absolute_error(test_y, predicted_leak_speed))
+        
+        leak_locations_model = HistGradientBoostingRegressor(random_state=1)
         leak_locations_model.fit(train_X, train_y)
         
         predicted_leak_speed = leak_locations_model.predict(test_X)
         
-        print("MAE:", mean_absolute_error(test_y, predicted_leak_speed))
+        mae = mean_absolute_error(test_y, predicted_leak_speed)
         
-        
-        
-        
-        for i in [5, 50, 500, 5000]:
-            mae = self.get_mae(i, train_X, test_X, train_y, test_y)
-            print(mae)
-
-        
-    def get_mae(self, max_leaf_nodes, train_X, val_X, train_y, val_y):
-        leak_locations_model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
-        leak_locations_model.fit(train_X, train_y)
-        
-        predicted_leak_speed = leak_locations_model.predict(val_X)
-        
-        mae = mean_absolute_error(val_y, predicted_leak_speed)
-        
-        return(mae)
-        
+        print(mae)
 
